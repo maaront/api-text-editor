675143c4b23e229dfd9c6f4c91fc520a4ea5965f
maaront <35611834+maaront@users.noreply.github.com>
2023-07-03 11:48:21 -0400

    set up module and rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,55 +1,55 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-const { default: editor } = require('./src/js/editor');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const { default: editor } = require("./src/js/editor");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     //Entry point for files to be bundled
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      editor: "./src/js/editor.js",
+      header: "./src/js/header.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       //webpack plugin to generate html file
       new HtmlWebpackPlugin({
-        template: './src/index.html',
-        title: 'Text Editor',
+        template: "./src/index.html",
+        title: "Text Editor",
       }),
 
       // ingect service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
-      
+
       // create manifest file
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        short_name: 'Editor',
-        description: 'A simple text editor',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        start_url: '/',
-        publicPath: './',
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "A simple text editor",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        start_url: "/",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve('src/images/icon-512x512.png'),
+            src: path.resolve("src/images/icon-512x512.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
@@ -57,7 +57,25 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // use babel loader in order to use ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
