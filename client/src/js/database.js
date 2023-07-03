9eb5aafb5761e634e0b5f15ba2659b12a3956044
maaront <35611834+maaront@users.noreply.github.com>
2023-07-03 15:12:56 -0400

    build get method for indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,38 +1,43 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 // Initialize the database
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
-
   });
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-// Open the db
+  // Open the db
   const db = await initdb();
-// Create a transaction
-  const tx = db.transaction('jate', 'readwrite');
-// Get the object store
-  const store = tx.objectStore('jate');
+  // Create a transaction
+  const tx = db.transaction("jate", "readwrite");
+  // Get the object store
+  const store = tx.objectStore("jate");
   // Add the content to the object store
   await store.add(content);
 
-  console.error('putDb not implemented');
-}
-
-
-
-
-
+  console.error("putDb not implemented");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Open the db
+  const db = await initdb();
+  // Create a transaction
+  const tx = db.transaction("jate", "readonly");
+  // Get the object store
+  const store = tx.objectStore("jate");
+  // Get all the content in the store
+  const content = await store.getAll();
+  console.log("Content fetched from jate database");
+  return content;
+};
 
 initdb();
