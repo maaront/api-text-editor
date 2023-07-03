9fdc68a8c3d705f3045ca19a7727e66af1ae8d71
maaront <35611834+maaront@users.noreply.github.com>
2023-07-03 11:41:35 -0400

    initial webpack plugin and sw set up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { default: editor } = require('./src/js/editor');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -9,16 +10,32 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+    //Entry point for files to be bundled
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      //webpack plugin to generate html file
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Text Editor',
+      }),
+
+      // ingect service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
       
+      
+
     ],
 
     module: {
