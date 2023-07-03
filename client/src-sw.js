f1b1d9071103cccd893b15e46a0291fafa8ecc04
maaront <35611834+maaront@users.noreply.github.com>
2023-07-03 16:00:37 -0400

    add some console logs for data validation

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,12 +27,12 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+// Implement asset caching
+registerRoute(
+  // Filter the requests we want to cache by type
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    // Name of the cache storage.
+    // Name of the cache storage
     cacheName: 'asset-cache',
     plugins: [
       // This plugin will cache responses with these headers to a maximum-age of 30 days
@@ -40,6 +40,5 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
         statuses: [0, 200],
       }),
     ],
-  });
-
-registerRoute();
+  })
+);
