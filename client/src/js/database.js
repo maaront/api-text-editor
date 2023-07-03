a8582aaefe7a9d86860df4e889d97e04358ff2dd
maaront <35611834+maaront@users.noreply.github.com>
2023-07-03 14:52:52 -0400

    build put method for indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// Initialize the database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -10,10 +11,26 @@ const initdb = async () =>
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
-  });
 
+  });
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+// Open the db
+  const db = await initdb();
+// Create a transaction
+  const tx = db.transaction('jate', 'readwrite');
+// Get the object store
+  const store = tx.objectStore('jate');
+  // Add the content to the object store
+  await store.add(content);
+
+  console.error('putDb not implemented');
+}
+
+
+
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
